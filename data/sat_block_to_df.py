14289bb28c762b31d7065f392f49e24cfd835f11
Kirill <k_vasuirin@mail.ru>
2023-05-21 19:42:28 +0300

    почти законченная реализация DataLoader-а

---
+++ data/sat_block_to_df.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+# data = open('1234.txt').read()
+def sat_block_to_df(sat_block):
+    first_line_marker = sat_block.find('\n')
+    first_line = sat_block[:first_line_marker]
+    agent_1, _, agent_2 = first_line.split('-')
+    second_line_marker = sat_block.find('-\n')
+    data_after_column_names = sat_block[sat_block.find('(sec)'):]
+    pure_data = data_after_column_names[data_after_column_names.find('-\n') + 2:]
+    nums = []
+    start_datetimes = []
+    end_datetimes = []
+    durs = []
+    try:
+        while True:
+            line = pure_data[:pure_data.find('\n')]
+            num, start_day, start_month, start_year, start_time, end_day, end_month, end_year, end_time, duration = line.split()
+            nums.append(num)
+            durs.append(duration)
+            start_datetimes.append(pd.to_datetime(start_day + ' ' + start_month + ' ' + start_year + ' ' + start_time))
+            end_datetimes.append(pd.to_datetime(end_day + ' ' + end_month + ' ' + end_year + ' ' + end_time))
+            pure_data = pure_data[pure_data.find('\n') + 1:]
+    except ValueError:
+        pass
+    df = pd.DataFrame.from_dict({
+        'access': nums,
+        'start_datetime': start_datetimes,
+        'end_datetime': end_datetimes,
+        'duration': durs
+    })
+    return agent_1, agent_2, df
+
+
+if __name__ == '__main__':
+    sat_block = open('msc_to_110301.txt').read()
+    a, b, c = sat_block_to_df(sat_block)
+    print()
